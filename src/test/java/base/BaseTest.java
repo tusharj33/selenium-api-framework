03102bc51b353e58db7d7ead76b5cd4c86dd9c4d
Tushar Joshi <tusharj33@gmail.com>
2026-06-14 22:39:16 +0530

    commit 3

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -4,14 +4,23 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.openqa.selenium.edge.EdgeDriver;
 
 public class BaseTest {
 
     protected WebDriver driver;
-
     @BeforeMethod
-    public void setup() {
-        driver = new ChromeDriver();
+    @Parameters("browser")
+    public void setup(String browser) {
+
+        if(browser.equalsIgnoreCase("chrome")) {
+            driver = new ChromeDriver();
+        }
+        else if(browser.equalsIgnoreCase("edge")) {
+            driver = new EdgeDriver();
+        }
+
         driver.manage().window().maximize();
     }
 
